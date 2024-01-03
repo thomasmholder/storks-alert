1b4e9719a73403aacc87aed2e18e8ee81877ab81
Thomas Holder <tholder7@gatech.edu>
2024-01-02 21:35:47 -0600

    Restored / prefix

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-prefix = '!'
+prefix = '/'
 
 intents = discord.Intents.all()
 activity = discord.Game(name="Pixel Cat's End")
